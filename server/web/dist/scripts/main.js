92b7f2d92a81721a1c0b9a8d84e10523a064ae79
jhemnes <jhemnes@gmail.com>
2016-02-02 21:13:52 -0700

    got the age selector up and running or something

--- server/web/dist/scripts/main.js
+++ server/web/dist/scripts/main.js
@@ -7,18 +7,20 @@
     angular
         .module('eligbl')
         .component('ageSelector', {
-            template: [
-                '<ul>',
-                    '<li ng-repeat="age in ctrl.allowableAges">{{age}}</li>',
-                '</ul>'
-            ].join(''),
+            templateUrl: './dist/templates/age-selector.html',
             controllerAs: 'ctrl',
             controller: function() {
                 var vm = this;
-                this.helloThingy = {name: "helloThingythinght"};
-                this.allowableAges = [];
+                vm.selectedAges = [];
+                vm.allowableAges = [];
                 for (let i = 0; i < 20; ++i) {
-                    this.allowableAges.push(i);
+                    vm.allowableAges.push(i);
+                }
+                vm.isActiveAge = function(age){
+                    return vm.selectedAges.indexOf(age) !== -1 ? 'btn btn-success' : 'btn btn-default';
+                };
+                vm.toggle = function(age){
+                    vm.selectedAges.indexOf(age) !== -1 ? vm.selectedAges.splice(vm.selectedAges.indexOf(age), 1) : vm.selectedAges.push(age);
                 }
             }
         });
