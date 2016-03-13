5d3e8d9e1d5dfc9847e70ce226b8f964d62acaec
jhemnes <jhemnes@gmail.com>
2016-03-13 11:24:43 -0600

    more bootstrappy

--- server/web/dist/scripts/main.js
+++ server/web/dist/scripts/main.js
@@ -68,16 +68,20 @@
 })();
 (function(){
     'use strict';
+    function personController(){
+        let vm = this;
+        vm.children = [];
+        vm.totalFamilySize = null;
+        vm.income = null;
+        vm.submit = function(){
+            $http.post()
+        }
+    }
     angular
         .module('eligbl')
         .component('person', {
             templateUrl: './dist/templates/person.html',
-            controllerAs: 'ctrl',
-            controller: function(){
-                let vm = this;
-                vm.children = [];
-                vm.totalFamilySize = null;
-                vm.income = null;
-            }
+            controller: personController,
+            controllerAs: 'ctrl'
         })
 })();
