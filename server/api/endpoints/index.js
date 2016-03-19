fb722370993e68fe9c0dc6c5135a3be55b226e15
jhemnes <jhemnes@gmail.com>
2016-03-19 16:17:09 -0600

    midway commit on get endpoint

--- server/api/endpoints/index.js
+++ server/api/endpoints/index.js
@@ -4,13 +4,35 @@ exports.register = function (server, options, next) {
         method: 'POST',
         path: '/getFilteredPrograms',
         handler: function (request, reply) {
-            var FederalAidPrograms = request.server.plugins['hapi-mongo-models'].Programs;
-            FederalAidPrograms.find({}, (err,result)=>{
+            const FederalAidPrograms = request.server.plugins['hapi-mongo-models'].Programs;
+            const person = request.payload;
+            const percentageOfFPL = calculateFPL(person.annualIncome, person.numberOfFamilyMembers);
+            const ageOfYoungestChild = getYoungestChild(person.children);
+            const filter = {
+
+            };
+            FederalAidPrograms.find(filter, (err, result)=> {
                 console.log(result);
                 return reply({result});
             });
         }
     });
+    const calculateFPL = (annualIncome, numberOfFamilyMembers) => {
+        const individualIncome = 11880;
+        const increment = 4140;
+        let fpl = individualIncome + (increment * (numberOfFamilyMembers - 1));
+        return annualIncome / fpl;
+    };
+    const getYoungestChild = (children) => {
+        //TODO: need to handle no children
+        let youngest = children[0];
+        children.map((child) => {
+            if (child < youngest) {
+                youngest = child;
+            }
+        });
+        return youngest;
+    };
 
     next();
 };
